26108e9c96b807f90b41e168702b8dbc96f819a9
Coleman Smith <csmitt23@gmail.com>
2018-05-31 08:50:09 -0400

    to_string to toString

--- contracts/budblocks-network/lib/logic.js
+++ contracts/budblocks-network/lib/logic.js
@@ -75,8 +75,8 @@ async function sendNote(note_info) {
     }
 
     let factory = getFactory();
-    let new_num = sender.num_notes_sent++;
-    let note = factory.newResource('org.budblocks', 'Note', sender.username.concat('.').concat(new_num.to_string()));
+    // let new_num = sender.num_notes_sent++;
+    let note = factory.newResource('org.budblocks', 'Note', sender.username.concat('.').concat((sender.num_notes_sent++).toString()));
     note.sender = factory.newRelationship('org.budblocks', 'Buddy', sender.username);
     note.receiver = factory.newRelationship('org.budblocks', 'Buddy', receiver.username);
     note.amount = note_info.amount;
